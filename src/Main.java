21fde84c792bad9d6f6b9bfe31af382d9d403061
damridfwdfwefd <alexdamsem2004@gmail.com>
2023-01-18 18:55:20 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            int ExeptionHandLing 10 / 0;
+        } catch (Exception e) {
+            System.out.println(.e.getMessage());
+        } catch (Exception e) { // поймать exeption (прерырвание, исключение)
+        } System.out.println(e.getMesssage());
+    }catch (Exception e) {
+        System.out.println(("Была ошибка в вводе числа");
+        System.out.println(e.getMessage());
+    }catch (Exception )
+
+        System.out.println("Exception Handking!");
+
+    }
+}
